7a6134d2f47a9d967a1af8ec4aec3156c4fa82e6
sathish16198 <35728168+sathish16198@users.noreply.github.com>
2018-02-01 15:46:05 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    char x;
+    printf("enter the charecter:");
+    scanf("%c" , &x);
+    if(x=='A'||x=='I'||x=='O'||x=='U'||x=='a'||x=='e'||x=='i'||x=='o'||x=='u')
+    {
+    printf("It is vowel");
+    }
+    else
+   {
+       printf("It is consonent");
+    }
+}
